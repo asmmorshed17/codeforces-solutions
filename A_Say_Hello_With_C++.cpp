153b7ffcd9696f22b6f38d20a1628ce0f0a0e4dd
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-01 09:54:28 +0600

    ICPC Assuit Uni. (Basic)

---
+++ A_Say_Hello_With_C++.cpp
@@ -0,0 +1,16 @@
+//ICPC Assiut University Community
+//Sheet- 1: (Data Types - Conditions)
+
+#include<bits/stdc++.h>
+using namespace std ;
+int main(){
+    string s;
+    cin>>s ;
+    cout<<"Hello, " <<s <<endl ;
+    return 0 ;
+}
+
+// morshed   
+// Hello, morshed
+
+//Basic 
