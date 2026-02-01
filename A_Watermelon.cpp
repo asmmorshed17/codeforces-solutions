fb5267e2f7683d472163a32fabe4ec4deeeb923a
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-01 09:45:38 +0600

    brute force, math, *800

---
+++ A_Watermelon.cpp
@@ -0,0 +1,14 @@
+//4A - Watermelon:
+
+#include<stdio.h>
+int main()
+{
+    int w;
+    scanf("%d",&w);
+   if( w>2 && w%2==0)
+    printf("YES\n");
+    else
+    printf("NO\n");
+}
+
+//brute force, math, *800
