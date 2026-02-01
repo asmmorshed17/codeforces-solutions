ede63aa7787c488d2de9182afee4b5d982380c49
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-01 09:58:52 +0600

    ICPC Assuit Uni. (Sh.-1: Data Types)

---
+++ D_Difference.cpp
@@ -0,0 +1,19 @@
+//ICPC Assiut University Community
+//Sheet- 1: (Data Types - Conditions)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a ;
+    long long b ;
+    long long c ;
+    long long d ;
+    cin>>a>>b>>c>>d ;
+    cout<<"Difference = "<<(a*b)-(c*d)<<endl;
+return 0;
+}
+
+/*
+1 2 3 4
+Difference = -10
+*/
