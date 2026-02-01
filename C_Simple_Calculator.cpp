98f7685e6302821af8b4b632795619dddcc11f30
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-01 09:58:04 +0600

    ICPC Assuit Uni. (Sh.-1: Data Types)

---
+++ C_Simple_Calculator.cpp
@@ -0,0 +1,22 @@
+//ICPC Assiut University Community
+//Sheet- 1: (Data Types - Conditions)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long X ;
+    long long Y;
+    cin>>X>>Y ;
+    cout<<X<<" + "<<Y <<" = "<<X+Y<<endl;
+    cout<<X<<" * "<<Y <<" = "<<X*Y <<endl ;
+    cout<<X<<" - "<<Y<<" = "<<X-Y<<endl;
+return 0;
+}
+/*
+2002
+3002
+-----------------------
+2002 + 3002 = 5004
+2002 * 3002 = 6010004
+2002 - 3002 = -1000
+*/
