d90e43a522baa4c0a198f8f0310132428634070d
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-01 09:56:21 +0600

    ICPC Assuit Uni. (Sh.-1: Data Types)

---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,23 @@
+//ICPC Assiut University Community
+//Sheet- 1: (Data Types - Conditions)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x ;
+    long long  y ;
+    char ch ;
+    float z ;
+    double p ;
+    cin>> x>>y>>ch>>z>>p ;
+    cout<<x <<endl<<y<<endl<<ch<<endl<<z<<endl<<p<<endl;
+    return 0;
+}
+//Data Types
+
+// 3 12345678912345 a 334.23 14049.30493
+// 3
+// 12345678912345
+// a
+// 334.23
+// 14049.3
