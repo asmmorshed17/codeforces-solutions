b5baea81cf73eb4777afbd3da5d47a731fc92560
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-06 09:21:47 +0600

    math, *800

---
+++ 1335_A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,36 @@
+
+
+
+/* Bismillahir Rahmanir Rahim */
+////1335_A_Candies_and_Two_Sisters.cpp
+//math, *800
+#include<bits/stdc++.h>
+using namespace std;
+
+#define testcase int t; cin >> t; for (int i= 1;i<=t;i++)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+
+
+void solve()
+{
+   long long  n;
+   cin>>n;
+
+    long long ans = (n-1)/2 ;
+    cout<<ans<<endl;
+
+}
+
+
+int main()
+{
+    optimize();
+    //  testcase{
+    // solve();
+    // }
+
+   // solve();
+   testcase solve();
+    return 0;
+}
