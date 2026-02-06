39ffbbab5343b975535d2b2bed5bf0c7feeb676d
asmmorshed17 <a.s.m.morshed2660.com>
2026-02-06 22:41:27 +0600

    implementation, *800

---
+++ 1807A_Plus_or_Minus.cpp
@@ -0,0 +1,35 @@
+/* Bismillahir Rahmanir Rahim */
+//
+//1807A_Plus_or_Minus.cpp
+
+//https://codeforces.com/contest/1807/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define testcase int t; cin >> t; for (int i= 1;i<=t;i++)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+
+
+void solve()
+{
+  int a,b,c ;
+  cin>>a>>b>>c ;
+
+  if(a+b==c)cout<<"+"<<endl;
+  else cout<<"-"<<endl;
+}
+
+
+int main()
+{
+    optimize();
+    //  testcase{
+    // solve();
+    // }
+
+   // solve();
+   testcase solve();
+    return 0;
+}
