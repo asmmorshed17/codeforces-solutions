8bf8e7882ecd59574416b6e9198bbd3e5557ade7
asmmorshed17 <a.s.m.morshed2660@gmail.com>
2026-02-16 01:06:39 +0600

    brute force, implementation, *800

---
+++ 1873A_Short_Sort.cpp
@@ -0,0 +1,37 @@
+
+/* Bismillahir Rahmanir Rahim */
+//https://codeforces.com/contest/1873/problem/A
+//1873A_Short_Sort.cpp
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define testcase int t; cin >> t; for (int i= 1;i<=t;i++)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define ll long long
+
+void solve()
+{
+   string str ;
+   cin>>str;
+   if(str=="bca" || str=="cab"){
+    cout<<"NO"<<endl;
+   }
+   else{
+    cout<<"YES"<<endl;
+   }
+}
+
+
+int main()
+{
+    optimize();
+    
+//    solve();
+   testcase solve();
+
+    return 0;
+}
+//Solved()
+//brute force, implementation, *800
