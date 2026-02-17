a9dce2adbf09563cea1f3ca8289acf2e89be8bb6
asmmorshed17 <a.s.m.morshed2660@gmail.com>
2026-02-17 22:28:10 +0600

    brute force, constructive algorithms, greedy, implementation, strings, *800

---
+++ 2065A_Skibidus_and_Amog_u.cpp
@@ -0,0 +1,35 @@
+
+
+
+//2065A_Skibidus_and_Amog_u.cpp
+//https://codeforces.com/contest/2065/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+#define testcase int t; cin >> t; for (int i= 1;i<=t;i++)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define ll long long
+ 
+void solve()
+{
+    string str ;
+    cin>>str ;
+
+    str.pop_back();
+    str.pop_back();
+
+    cout<<str <<"i"<<endl;
+
+}
+ 
+
+int main()
+{
+    optimize();
+    
+//    solve();
+   testcase solve();
+
+}
+
+//brute force, constructive algorithms, greedy, implementation, strings, *800
